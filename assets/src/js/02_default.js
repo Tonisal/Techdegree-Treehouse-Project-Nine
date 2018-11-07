69289561105fe3519dc8351ddef2466184a77250
mbressel <markus.bressel@schalk-and-friends.de>
2018-11-07 18:55:02 +0100

    add current status

--- assets/src/js/02_default.js
+++ assets/src/js/02_default.js
@@ -1,4 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
     console.log('hello');
     console.log('hello');
+
+
+    $('.a-btn--settings-switch').click(function () {
+        console.log('click');
+        $(this).toggleClass('off');
+    });
 });
