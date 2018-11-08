019f002fbba9634139d91334a56a000e87db42ad
Tonisal <markus.bressel@gmx.de>
2018-11-08 08:33:26 +0100

    ad current status

--- assets/src/js/02_default.js
+++ assets/src/js/02_default.js
@@ -1,10 +1,56 @@
-$(document).ready(function () {
-    console.log('hello');
-    console.log('hello');
-
+function toggleSettingsSwitch() {
 
     $('.a-btn--settings-switch').click(function () {
         console.log('click');
         $(this).toggleClass('off');
     });
+}
+
+
+
+$(document).ready(function () {
+    toggleSettingsSwitch();
+
+    var ctx = document.getElementById("traffic").getContext('2d');
+    var myChart = new Chart(ctx, {
+        responsive: true,
+        type: 'line',
+        data: {
+            labels: ["16-22", "23-29", "6-12", "13-19", "20-26", "27-3", "4-10", "11-17", "18-24", "25-31"],
+            datasets: [{
+                label: '# of Votes',
+                data: [750, 1250, 1000, 1500, 2000, 1500, 1750, 1250, 1750, 2250, 1750, 2250],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+
+
+
 });
